0c25adcb99a720ba92baf9cb772124cebe1632b4
Natalie Gates <NatalieLGates05@gmail.com>
2026-03-30 20:50:09 -0500

    if a previously saved name is loaded, the name input field is now filled in with it

--- Assets/Scripts/StartMenuUIHandler.cs
+++ Assets/Scripts/StartMenuUIHandler.cs
@@ -10,10 +10,17 @@ using UnityEditor;
 public class StartMenuUIHandler : MonoBehaviour
 {
     public TextMeshProUGUI highscoreText;
+    public TMP_InputField nameInputField;
 
     private void Start()
     {
         UpdateHighscoreText();
+
+        // if there is a previously saved name, fill in the input field with it
+        string playerName = HighscoreHandler.Instance.playerName;
+        if (playerName.Length > 0) {
+            nameInputField.text = playerName;
+        }
     }
 
     public void StartGame()
